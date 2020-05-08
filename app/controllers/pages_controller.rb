68acdc78f4b216da5c63e8d925aea5860feaf2cc
SamDbs <dbs.samantha@gmail.com>
2020-05-08 23:50:14 +0200

    creation route, controller et view

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+require 'rss'
+require 'open-uri'
+
+
+class PagesController < ApplicationController
+
+    def index
+
+        # @articlesDev = Article.last(10)
+        # respond_to do |format|
+        #format.html
+        #format.rss { render :layout => false }
+        #end 
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#lecteur_flux_rss'
+  get '/lecteur', to: 'pages#lecteur_flux_rss'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
